489860fc1d1387e0b3645e69d333c1b4ac6c502e
am1nNP <amin79nadipoor@gmail.com>
2025-06-30 13:41:21 +0330

    refactor card and main code
    rewrite the code in async await way and best practice

--- react/src/assets/components/Card.jsx
+++ react/src/assets/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Card = ({ product: { id, title, image ,description,price} }) => {
+  
   return (
     <div className='box'>
         <div className='imageBox'>
@@ -9,7 +10,7 @@ const Card = ({ product: { id, title, image ,description,price} }) => {
         <h3 className='title'>{title}</h3>
         <p className='description'>{description}</p>
         <p className='price'>price :{price}$</p>
-        <button className='btn-purchase'>purchase</button>
+        <button onClick={()=>console.log("purchased")} className='btn-purchase'>purchase</button>
     </div>
   )
 }
--- react/src/assets/components/Main.jsx
+++ react/src/assets/components/Main.jsx
@@ -1,18 +1,32 @@
 import Card from "./Card";
 import "./Main.css";
-import React, { useState , useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 const Main = () => {
   const [productsList, setProductsList] = useState([]);
-//   const [searchTerm, setSearchTerm] = useState([]);
-  const fetchProducts = fetch("https://fakestoreapi.com/products/")
-    .then((res) => res.json())
-    .then((res)=> setProductsList(res))
-    
+  async function getProducts() {
+    try {
+      const res = await fetch("https://fakestoreapi.com/products");
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const data = await res.json();
+      setProductsList(data);
+      console.log(data);
+    } catch (err) {
+      console.log("there is a problem", err);
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
       <div className="container">
-        {productsList.map((product)=><Card key={product.id} product={product}/>)}
-        
+        {productsList.map((product) => (
+          <Card key={product.id} product={product} />
+        ))}
       </div>
     </>
   );
